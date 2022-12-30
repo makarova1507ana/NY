bbfb22410259b61cc4b4a3aac3e62813221b039b
makarova1507ana <103330304+makarova1507ana@users.noreply.github.com>
2022-12-30 19:46:29 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,24 @@
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 5000); // Меняйте изображение каждые 2 секунды
+}
+
+// Вперед/назад элементы управления
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Элементы управления миниатюрами изображений
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
